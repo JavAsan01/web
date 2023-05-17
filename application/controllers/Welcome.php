b010a5e471fda620e456120a2667b94e77862319
JavAsan01 <carlos.asanza0967@utc.edu.ec>
2023-05-17 11:43:35 -0500

    update2

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -2,8 +2,6 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
-	// HOla
-
 	/**
 	 * Index Page for this controller.
 	 *
@@ -21,6 +19,8 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
+		$this->load->view('header');
 		$this->load->view('welcome_message');
+		$this->load->view('footer');
 	}
 }
--- application/views/instructores/nuevo.php
+++ application/views/instructores/nuevo.php
@@ -1,1 +1,1 @@
- 
+<h1>NUEVO INSTRUCTOR</h1> 

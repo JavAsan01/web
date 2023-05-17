0376561b040e6290d4bae2ffb13960d01ff7ea3f
JavAsan01 <carlos.asanza0967@utc.edu.ec>
2023-05-17 12:03:27 -0500

    update3

--- application/views/header.php
+++ application/views/header.php
@@ -32,20 +32,20 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li><a href="<?php echo site_url();?>/menus/desayunos">Desayunos</a></li>
+        <!-- <li><a href="<?php echo site_url();?>/menus/desayunos">Desayunos</a></li>
         <li><a href="<?php echo site_url();?>/menus/almuerzos">Almuerzos</a></li>
         <li><a href="<?php echo site_url();?>/menus/meriendas">Meriendas</a></li>
-        <li><a href="<?php echo site_url();?>/menus/cartas">Platos a la Carta</a></li>
-        <!--<li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+        <li><a href="<?php echo site_url();?>/menus/cartas">Platos a la Carta</a></li> -->
+        <li class="INSTRUCTOR">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">INSTRUCTORES <span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
+            <li><a href="<?php echo site_url();?>/instructores/nuevo">Nuevo</a></li>
+            <li><a href="<?php echo site_url();?>/instructores/index">Listado</a></li>
+            <!-- <li><a href="#">Something else here</a></li>
             <li role="separator" class="divider"></li>
             <li><a href="#">Separated link</a></li>
             <li role="separator" class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
+            <li><a href="#">One more separated link</a></li> -->
           </ul>
         </li>
       </ul>
